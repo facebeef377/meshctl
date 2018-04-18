cb380b7345ea0eca3d386666612a2c0e41522e6a
lukaszobrebski <lukasz.obrebski95@gmail.com>
2018-04-19 01:10:34 +0200

    Dodano: funkcje provision (do przetestowania w meshctl). Klase test_provision do testowania parsowania i uzupelniania bazy danych (klase dodano rowniez do resources

--- meshctl.py
+++ meshctl.py
@@ -55,6 +55,26 @@ class Meshctl:
         return "Provisioned!"
         
     def provision(self, device_name, uuid, address):
+        #Wyslanie polecenia meshctl
+        self.child.send("security 0" + "\n")
+        time.sleep(1)
+        self.child.send("provision " + uuid + "\n")
+        time.sleep(12)
+        
+        #Otwarcie pliku z danymi przeslanymi przez nrf
+        file = open("prov_db.json", 'r')
+        content = file.read()
+        content= "{\"data\":[" + content + "]}"
+        content = json.loads(content)     
+        unicastAddress = content['data'][-1]['nodes'][-1]['configuration']['elements'][-1]['unicastAddress']
+        
+        #Uzupelnienie bazy danych o dane z pliku
+        query_string = "INSERT INTO devices (id, address, name, state, target, uuid)"
+        query_string += "VALUES (NULL, NULL, \"zephyr\", 0,"
+        query_string += unicastAddress + ", \"" + uuid + "\" )"
+        conn = db_connect.connect()
+        query = conn.execute(query_string)
+        
         return 0
     
     '''Przemyśleć to czy właściwie jest potrzebne'''
@@ -90,7 +110,23 @@ class Meshctl:
         time.sleep(1)
         return "LED Off!"
     
-    
+class test_provisioning(Resource):
+    def get(self):
+        uuid = "129381241298714edfa"
+        file = open("prov_db.json", 'r')
+        content = file.read()
+        content= "{\"data\":[" + content + "]}"
+        content = json.loads(content)     
+        unicastAddress = content['data'][-1]['nodes'][-1]['configuration']['elements'][-1]['unicastAddress']
+        #Uzupelnienie bazy danych o dane z pliku
+        query_string = "INSERT INTO devices (id, address, name, state, target, uuid)"
+        query_string += "VALUES (NULL, NULL, \"zephyr\", 0,"
+        query_string += unicastAddress + ", \"" + uuid + "\" )"
+        conn = db_connect.connect()
+        query = conn.execute(query_string)
+        
+        return unicastAddress
+
 class UserList(Resource):
     def post(self):
         conn = db_connect.connect()
@@ -146,7 +182,7 @@ api.add_resource(addlight, '/api/addlight') # Dodaje plytke do bazy
 api.add_resource(devicelist, '/api/devices') # Zwraca liste wszystkich urzadzen
 #hece z plytka
 #api.add_resource(writeled, '/writeled') #Zapala leda na NRF 
-
+api.add_resource(test_provisioning, '/api/test_provisioning') #Do testowania 
 
 if __name__ == '__main__':
     print("Init mshctl...")
